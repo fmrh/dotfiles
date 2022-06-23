487e86e56b29fc6dde5711cd2016274f9b89b77f
Fillip Holgersen <fmrh@pm.me>
2022-06-23 11:47:00 +0100

    configured settings and extensions for vscode

---
+++ users/fmrh/vscode/configs/default.nix
@@ -0,0 +1,55 @@
+{
+  "editor.tabSize" = 2;
+  "editor.bracketPairColorization.enabled" = true;
+  "editor.guides.bracketPairs" = "active";
+  "editor.letterSpacing" = 0.2;
+  "editor.lineHeight" = 1.5;
+  "editor.insertSpaces" = true;
+  "editor.autoClosingBrackets" = "languageDefined";
+  "editor.autoIndent" = "full";
+  "editor.fontFamily" = "Hasklug Nerd Font";
+  "editor.fontLigatures" = true;
+  "editor.fontSize" = 14;
+  "editor.defaultFormatter" = "esbenp.prettier-vscode";
+  "editor.formatOnPaste" = true;
+  "editor.formatOnSave" = true;
+  "editor.cursorStyle" = "underline";
+  "editor.cursorBlinking" = "phase";
+  "editor.mouseWheelZoom" = true;
+  "editor.minimap.renderCharacters" = false;
+  "editor.minimap.maxColumn" = 200;
+  "editor.minimap.showSlider" = "always";
+  "editor.minimap.side" = "right";
+  "editor.renderWhitespace" = "boundary";
+  "editor.smoothScrolling" = true;
+  "editor.linkedEditing" = true;
+  "editor.cursorSmoothCaretAnimation" = true;
+
+  "workbench.editor.highlightModifiedTabs" = true;
+  "workbench.iconTheme" = "material-icon-theme";
+  "workbench.sideBar.location" = "left";
+  "workbench.settings.enableNaturalLanguageSearch" = false;
+  "workbench.colorTheme" = "Ayu Dark Bordered";
+  "workbench.list.smoothScrolling" = true;
+
+  "files.trimFinalNewlines" = true;
+  "files.insertFinalNewline" = true;
+  "files.trimTrailingWhitespace" = true;
+
+  "window.autoDetectHighContrast" = false;
+  "window.autoDetectColorScheme" = false;
+  "window.title" = "\${rootName}\${separator}\${activeEditorMedium} \${dirty}";
+  "window.titleSeparator" = " | ";
+
+  "explorer.confirmDragAndDrop" = false;
+  "explorer.confirmDelete" = false;
+  "explorer.openEditors.visible" = 0;
+
+  "terminal.integrated.cursorStyle" = "underline";
+  "terminal.integrated.profiles.linux" = {
+    "zsh" = { "path" = "zsh"; };
+  };
+
+  "update.mode" = "none";
+  "telemetry.telemetryLevel" = "off";
+}
--- users/fmrh/vscode/default.nix
+++ users/fmrh/vscode/default.nix
@@ -1,7 +1,33 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
+let
+  myExtentions = with pkgs.vscode-extensions; [
+    # Themes
+    pkief.material-icon-theme
+    # teabyii.ayu
+
+    # Language support
+    jnoortheen.nix-ide
+    yzhang.markdown-all-in-one
+    dbaeumer.vscode-eslint
+
+
+    # Testing
+    ritwickdey.liveserver
+
+    # QOL
+    # vscodevim.vim
+    esbenp.prettier-vscode
+    formulahendry.auto-rename-tag
+    eamodio.gitlens
+  ];
+
+  mySettings = builtins.readFile ./configs
+
+in {
   programs.vscode = {
     enable = true;
+    extensions = myExtentions;
+    userSettings = mySettings;
   };
 }
