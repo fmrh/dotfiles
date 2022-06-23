a9f7c60a4368f031e60d0b88b096e583e7b83c13
Fillip Holgersen <fmrh@pm.me>
2022-06-23 09:12:50 +0100

    configured colours in alacritty

--- users/common/terminal/default.nix
+++ users/common/terminal/default.nix
@@ -20,6 +20,36 @@
           blinking = "On";
         };
       };
+      colors = {
+        primary = {
+          background = "#000000";
+          foreground = "#d5c4a1";
+        };
+        cursor = {
+          text = "#000000";
+          cursor = "#d5c4a1";
+        };
+        normal = {
+          black   = "#000000";
+          red     = "#fb4934";
+          green   = "#b8bb26";
+          yellow  = "#fabd2f";
+          blue    = "#83a598";
+          magenta = "#d3869b";
+          cyan    = "#8ec07c";
+          white   = "#d5c4a1";
+        };
+        bright = {
+          black   = "#665c54";
+          red     = "#fe8019";
+          green   = "#3c3836";
+          yellow  = "#504945";
+          blue    = "#bdae93";
+          magenta = "#ebdbb2";
+          cyan    = "#d65d0e";
+          white   = "#fbf1c7";
+        };
+      };
       font = {
         normal = {
           family = "Hasklug Nerd Font";
