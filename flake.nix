8459286c98d6ad1a3f84715780296a1a2508ff8b
Fillip Holgersen <fmrh@pm.me>
2022-06-23 10:56:59 +0100

    Enabled firefox extensions via NUR

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nur = { url = "github:nix-community/NUR"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = 
+  outputs =
   { self
   , nixpkgs
   , home-manager
+  , nur
   , ... } @ inputs:
-    
+
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -23,14 +25,14 @@
     in {
       nixosConfigurations = (
         import ./hosts {
-          inherit lib pkgs inputs system;
-	      }	
+          inherit lib system pkgs inputs;
+	      }
       );
       homeConfigurations = (
         import ./users {
-         inherit lib system pkgs home-manager;
+         inherit lib system pkgs inputs;
         }
       );
     };
-    
+
 }
--- users/fmrh/browser/default.nix
+++ users/fmrh/browser/default.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
-{
+let
+  myExtensions = with pkgs.nur.repos.rycee.firefox-addons; [
+    ublock-origin
+  ];
+
+in {
   programs.firefox = {
     enable = true;
   };
